e090b162117793174c1415b81a601c1a4f623663
Théo Constant <to.constant@gmail.com>
2025-04-21 01:01:26 +0200

    Detect player presence + avoid jitter by moving player itself

--- LudumDare57/Assets/Code/Scripts/Elevator.cs
+++ LudumDare57/Assets/Code/Scripts/Elevator.cs
@@ -3,12 +3,64 @@ using UnityEngine;
 
 public class Elevator : MonoBehaviour
 {
-    [SerializeField] private float _levelHeight;
+    [SerializeField] private float _playerDetectionDelay;
+    [SerializeField] private float _height;
     [SerializeField] private float _travelTime;
     [SerializeField] private Ease _easeType;
+    [SerializeField] private Collider _collider;
 
-    private void Awake()
+    float _playerTimer;
+    bool _isMoving;
+
+    private void MoveUp()
     {
-        transform.DOLocalMoveY(-_levelHeight, _travelTime).SetEase(_easeType).SetLoops(-1, LoopType.Yoyo);
+        _isMoving = true;
+        DOTween.Sequence()
+            .Join(transform.DOLocalMoveY(transform.position.y + _height, _travelTime).SetEase(_easeType))
+            .Join(Game.Player.DOLocalMoveY(Game.Player.position.y + _height, _travelTime).SetEase(_easeType))
+            .OnComplete(Stop);
+    }
+
+    private void MoveDown()
+    {
+        _isMoving = true;
+        DOTween.Sequence()
+            .Join(transform.DOLocalMoveY(transform.position.y - _height, _travelTime).SetEase(_easeType))
+            .Join(Game.Player.DOLocalMoveY(Game.Player.position.y - _height, _travelTime).SetEase(_easeType))
+            .OnComplete(Stop);
+    }
+
+    private void Stop()
+    {
+        _isMoving = false;
+    }
+
+    private void CheckPlayerPosition()
+    {
+        if (_isMoving)
+        {
+            return;
+        }
+
+        if (Game.Player.position.x > _collider.bounds.min.x && Game.Player.position.x < _collider.bounds.max.x
+                && Game.Player.position.z > _collider.bounds.min.z && Game.Player.position.z < _collider.bounds.max.z)
+        {
+            _playerTimer += Time.deltaTime;
+
+            if (_playerTimer > _playerDetectionDelay)
+            {
+                MoveUp();
+                _playerTimer = 0f;
+            }
+        }
+        else
+        {
+            _playerTimer = 0f;
+        }
+    }
+
+    private void Update()
+    {
+        CheckPlayerPosition();
     }
 }
